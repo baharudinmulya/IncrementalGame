c47a4f7f1d8e93964253eb2742b0c2484f6b7f7b
baharudinmulya <baharu12185173@bsi.ac.id>
2021-09-10 14:45:26 +0700

    masuk pertama

---
+++ Assets/Scripts/TapArea.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+using UnityEngine.EventSystems;
+
+ 
+
+public class TapArea : MonoBehaviour, IPointerDownHandler
+
+{
+    public AudioSource gameSfx;
+
+    // void Start()
+    // {
+    //     source = GetComponent<AudioSource>();
+    // }
+
+    public void OnPointerDown (PointerEventData eventData)
+
+    {
+
+        GameManager.Instance.CollectByTap (eventData.position, transform);
+        gameSfx.Play();
+    }
+
+}
